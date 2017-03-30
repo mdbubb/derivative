7fe714c09c30e00a56cdb15f166c92a1af51c01d
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-03-30 16:41:17 -0400

    product rule is done

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your function...Please use X as your variable and use ^ to denote exponents");
+        System.out.println("Enter your function...Please use X as your variable and use ^ to denote exponents, use ( and ) to denote a product EXAMPLE: 2x^4(5x^3) or cos x (sin x)");
         String func = input.nextLine();
         System.out.println("Your derivative is: ");
-        one.solve(func);
+        one.powerRule(func);
     }
 }
